c53eef8248fb11ed168eaffbc45abd94d6e66541
GangaKatte <gangakatte883@gmail.com>
2025-03-19 23:19:46 +0530

    Add files via upload

--- max_product_subarray.py
+++ max_product_subarray.py
@@ -7,3 +7,27 @@ for i in range(1,len(nums)):
     max_product=max(max_product,current_product)
 
 print(max_product)    
+
+
+#way2
+class Solution(object):
+    def maxProduct(self, nums):
+        if not nums:
+            return 0  # Edge case: empty array
+
+        max_product_so_far = nums[0]
+        min_product = nums[0]
+        max_product = nums[0]
+
+        for i in range(1, len(nums)):
+            num = nums[i]
+
+            if num < 0:  # Swap max and min when a negative number appears
+                min_product, max_product = max_product, min_product
+
+            max_product = max(num, max_product * num)
+            min_product = min(num, min_product * num)
+
+            max_product_so_far = max(max_product_so_far, max_product)
+
+        return max_product_so_far
